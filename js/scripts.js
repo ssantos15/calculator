f7ff6c415a269ef53cccdfa3e2c5a64967978d8e
Padma Patil <padma@email.com>
2018-08-09 07:32:31 -0700

    This will have another js file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,25 @@
+var mode = prompt("Please type in an operation (+, -, *, and /): ");
+var num1 =  parseInt(prompt("Please type in a number."));
+var num2 = parseInt(prompt("Please type in a second number."));
+
+
 var add = function (num1, num2){
 return num1+num2;
 };
 
-var num1 = parseInt(prompt("Enter a number:"));
-var num2 = parseInt(prompt("Enter another number:"));
+var subtract = function(num1, num2){
+  return num1-num2;
+};
+
+var multiply = function(num1, num2){
+  return num1*num2;
+};
+
+var divide = function(num1, num2){
+  return num1/num2
+}
+
 alert(add(num1,num2));
+alert(subtract(num1,num2));
+alert(multiply(num1,num2));
+alert(divide(num1,num2));
