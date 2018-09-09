3938bd8576117e1b41b57f9b75e4e010a7015238
Padma Patil <padma@email.com>
2018-09-08 18:57:55 -0700

    Redid everything. Going to make the calculator more advance in the near future where it'll allow user to decide the operator.

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,24 @@
-var mode = prompt("Please type in an operation (+, -, *, and /): ");
-var num1 =  parseInt(prompt("Please type in a number."));
-var num2 = parseInt(prompt("Please type in a second number."));
-
-
-var add = function (num1, num2){
-return num1+num2;
+var add = function(num1, num2) {
+  return num1 + num2;
 };
 
-var subtract = function(num1, num2){
+var subtract = function(num1, num2) {
   return num1-num2;
 };
 
-var multiply = function(num1, num2){
+var multiply = function(num1,num2) {
   return num1*num2;
 };
 
-var divide = function(num1, num2){
-  return num1/num2
-}
+var divide = function(num1,num2) {
+  return num1/num2;
+};
 
-alert(add(num1,num2));
-alert(subtract(num1,num2));
-alert(multiply(num1,num2));
-alert(divide(num1,num2));
+
+var inputNum1 = parseInt(prompt("Please enter the first number:"));
+var inputNum2 = parseInt(prompt("Please enter the second number:"));
+
+alert(add(inputNum1, inputNum2));
+alert(subtract(inputNum1,inputNum2));
+alert(multiply(inputNum1,inputNum2));
+alert(divide(inputNum1,inputNum2));
